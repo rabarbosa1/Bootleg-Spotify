a5ce6e1357b50a1c7f7cec60df48486b88143400
Garrett <garrettleegrahamanderson@gmail.com>
2022-12-19 17:12:45 -0500

    playing with the api endpoints to get artist uri and then get the artist radio playlist. input a embed script in the script.js but not getting that to work. found a link to an iframe to get the playlist to embed with an iframe and a src tag. added notes to what needs to be done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 function getMusicalArtist() {
     const options = {
         method: 'GET',
@@ -7,7 +9,7 @@ function getMusicalArtist() {
         }
     };
     
-    fetch('https://genius-song-lyrics1.p.rapidapi.com/search?q=Alan%20Walker&per_page=10&page=1', options)
+    fetch('https://genius-song-lyrics1.p.rapidapi.com/search?q=the%20beatles&per_page=10&page=1', options)
         .then(function (response) {
             console.log(response)
             return response.json()
@@ -21,7 +23,7 @@ function getMusicalArtist() {
 }
 
 
-function getArtistPlaylist() {
+function getArtistForPlaylist() {
     const options = {
         method: 'GET',
         headers: {
@@ -30,7 +32,7 @@ function getArtistPlaylist() {
         }
     };
     
-    fetch('https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=10&numberOfTopResults=5', options)
+    fetch('https://spotify23.p.rapidapi.com/search/?q=queen&type=artists&offset=0&limit=1&numberOfTopResults=5', options)
         .then(function (response) {
             console.log(response)
             return response.json()
@@ -43,4 +45,59 @@ function getArtistPlaylist() {
         });
 }
 
+function getArtistPlaylist() {
+    var apiURL = 'https://spotify23.p.rapidapi.com/seed_to_playlist/?uri=spotify%3Aartist%3A1dfeR4HaWDbWqFHLkxsg1d'
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'bf414946damshfe6a477585380f8p1bd8e7jsnecd224242b38',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+    };
+    
+    fetch(apiURL, options)
+        .then(function (response) {
+            console.log(response)
+            return response.json()
+        })
+        .then(function(data) {
+            console.log(data)
+        })
+        .catch(function(err) {
+            console.error(err)
+        });
+
+}
 // getArtistPlaylist()
+
+// embed spotify player in app
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    let element = $('#embed-iframe');
+    let options = {
+        uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
+      };
+    let callback = (EmbedController) => {
+        console.log(EmbedController, 'testing callback')
+    };
+    IFrameAPI.createController(element, options, callback);
+  };
+
+
+
+
+
+// TO DO
+
+// need to query the genius api for Artist from search input
+// query genius api for songs by artist
+// query genius api for song lyrics??
+// need to create a function that iterates over the artist search input value to replace the spaces with '%20'
+
+// need to query the spotify api for artist from search input
+    // need to get the artist uri
+// need to query spotify api with the artist uri
+// need to create a function that iterates the artist uri and replaces colons (:) with '%3A'
+    // save in a variable to add to api endpoint as query param
+//
+
+
